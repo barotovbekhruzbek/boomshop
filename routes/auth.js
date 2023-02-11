555cbebc666a7e36ab18edea806b6828c60bad23
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-02-11 12:29:28 +0500

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,27 @@
+import {Router} from 'express'
+const router = Router()
+
+router.get('/login', (req, res) => {
+	res.render('login', {
+		title: "Login | Bexa",
+		isLogin:true
+	})
+})
+
+router.get('/register', (req, res) => {
+	res.render('register', {
+		title: "Register || Bexa",
+		isRegister:true
+	})
+})
+
+router.post('/login',(req,res)=> {
+	console.log(req.body);
+	res.redirect('/')
+})
+router.post('/register',(req,res)=> {
+	console.log(req.body);
+	res.redirect('/')
+})
+
+export default router
---
+++ routes/products.js
@@ -0,0 +1,25 @@
+import {Router} from 'express'
+const router = Router()
+
+router.get('/', (req, res) => {
+	res.render('index', {
+		title: 'boom shoop || bexa',
+
+	})
+})
+
+router.get('/products', (req, res) => {
+	res.render('products', {
+		title: "products",
+		isProducts:true
+	})
+})
+
+router.get('/add', (req, res) => {
+	res.render('add', {
+		title: 'add products',
+		isAdd: true
+	})
+})
+
+export default router

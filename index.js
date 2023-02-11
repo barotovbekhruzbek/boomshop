ae1940f233a9f801c3d9864fc67a20ce1653f21d
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-02-11 21:28:35 +0500

    mongo db

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import {create} from 'express-handlebars'
+import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
 import AuthRoutes from './routes/auth.js'
 import ProductsRoutes from './routes/products.js'
-
+dotenv.config()
 const app = express()
 
 const hbs = create({defaultLayout: 'main', extname: 'hbs'})
@@ -12,9 +14,21 @@ app.set('view engine', 'hbs')
 app.set('views', './views')
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
+app.use(express.json())
 
 app.use(AuthRoutes)
 app.use(ProductsRoutes)
+mongoose.set(`strictQuery`,false)
+mongoose.connect(
+ process.env.MONGO_URL,
+ {
+ useNewUrlParser: true
+
+}, ()=> console.log('Mongo DB connected'))
 
 const PORT = process.env.PORT || 4100
+
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+
+
+

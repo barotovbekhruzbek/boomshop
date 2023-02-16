398fbdc49ebadbe3445afda03c49df0a15e08ba1
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-02-16 16:05:13 +0500

    register Model

--- models/User.js
+++ models/User.js
@@ -1,11 +1,10 @@
-import {Schema,model} from "mongoose";
+import {Schema, model} from 'mongoose'
 
 const UserSchema = new Schema({
- firstname: {type: String, required:true},
- lastName : {type: String, required:true},
- email: {type: String, required: true, unique:true},
- password: {type: String, required: true}
-
+	firstName: {type: String, required: true},
+	lastName: {type: String, required: true},
+	email: {type: String, required: true, unique: true},
+	password: {type: String, required: true},
 })
 
 const User = model('User', UserSchema)
--- routes/auth.js
+++ routes/auth.js
@@ -4,31 +4,32 @@ const router = Router()
 
 router.get('/login', (req, res) => {
 	res.render('login', {
-		title: "Login | Bexa",
-		isLogin:true
+		title: 'Login | Sammi',
+		isLogin: true,
 	})
 })
 
 router.get('/register', (req, res) => {
 	res.render('register', {
-		title: "Register || Bexa",
-		isRegister:true
+		title: 'Register | Sammi',
+		isRegister: true,
 	})
 })
 
-router.post('/login',(req,res)=> {
-	console.log(req.body);
+router.post('/login', (req, res) => {
+	console.log(req.body)
 	res.redirect('/')
 })
-router.post('/register', async (req,res)=> {
-const UserData = {
-		firstname:req.body.firstname,
-		lastName:req.body.lastName,
-		email:req.body.email,
-		password:req.body.password
+
+router.post('/register', async (req, res) => {
+	const userData = {
+		firstName: req.body.firstname,
+		lastName: req.body.lastname,
+		email: req.body.email,
+		password: req.body.password,
 	}
-const user = await User.create(UserData)
-console.log(user);
+	const user = await User.create(userData)
+	console.log(user);
 	res.redirect('/')
 })
 

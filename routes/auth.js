a101aa9c426958344d31f60b687f0e27e55d9066
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-02-16 16:18:39 +0500

    hash password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 import {Router} from 'express'
 import User from '../models/User.js'
+import bcrytp from 'bcrypt'
 const router = Router()
 
 router.get('/login', (req, res) => {
@@ -22,11 +23,12 @@ router.post('/login', (req, res) => {
 })
 
 router.post('/register', async (req, res) => {
+	const hashedPassword = await bcrytp.hash(req.body.password,10)
 	const userData = {
 		firstName: req.body.firstname,
 		lastName: req.body.lastname,
 		email: req.body.email,
-		password: req.body.password,
+		password: hashedPassword,
 	}
 	const user = await User.create(userData)
 	console.log(user);

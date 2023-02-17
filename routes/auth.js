39f0f80b61e06518662fe4677a9f6b9f3d0fdbfd
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-02-17 15:01:25 +0500

    logout

--- routes/auth.js
+++ routes/auth.js
@@ -1,71 +1,85 @@
 import {Router} from 'express'
 import User from '../models/User.js'
-import bcrytp from 'bcrypt'
-import { generateJwtToken } from '../services/token.js'
+import bcrypt from 'bcrypt'
+import {generateJWTToken} from '../services/token.js'
+
 const router = Router()
 
 router.get('/login', (req, res) => {
+	if (req.cookies.token) {
+		res.redirect('/')
+		return
+	}
 	res.render('login', {
-		title: 'Login | bexa',
+		title: 'Login | Sammi',
 		isLogin: true,
-		loginError: req.flash("loginError")
+		loginError: req.flash('loginError'),
 	})
 })
 
 router.get('/register', (req, res) => {
+	if (req.cookies.token) {
+		res.redirect('/')
+		return
+	}
 	res.render('register', {
-		title: 'Register | bexa',
+		title: 'Register | Sammi',
 		isRegister: true,
-		registerError: req.flash("registerError"),
+		registerError: req.flash('registerError'),
 	})
 })
-router.get("/logout", (req,res)=> {
-	res.clearCookie("token")
-	res.send('Logout')
-})
-router.post('/login', async (req, res) => {
-	const {email,password}  = req.body
 
-	if(!email || !password) {
+router.get('/logout', (req, res) => {
+	res.clearCookie('token')
+	res.redirect('/')
+})
+
+router.post('/login', async (req, res) => {
+	const {email, password} = req.body
+
+	if (!email || !password) {
 		req.flash('loginError', 'All fields is required')
 		res.redirect('/login')
 		return
 	}
-const existUser = await User.findOne({email})
-if(!existUser) {
-	req.flash('loginError', 'User not found')
-	res.redirect('/login')
-	return
-	
-}
 
-const isPassEqual = await bcrytp.compare(password, existUser.password)
-if(!isPassEqual)  {
-	req.flash('loginError', 'Password Wrong')
-	res.redirect('/login')
-	return
-	
-}
-const token = generateJwtToken(existUser._id)
-res.cookie("token",token, {httpOnly:true ,secure:true})
-console.log(existUser);
+	const existUser = await User.findOne({email})
+	if (!existUser) {
+		req.flash('loginError', 'User not found')
+		res.redirect('/login')
+		return
+	}
+
+	const isPassEqual = await bcrypt.compare(password, existUser.password)
+	if (!isPassEqual) {
+		req.flash('loginError', 'Password wrong')
+		res.redirect('/login')
+		return
+	}
+
+	const token = generateJWTToken(existUser._id)
+	res.cookie('token', token, {httpOnly: true, secure: true})
 	res.redirect('/')
 })
 
 router.post('/register', async (req, res) => {
-	const {firstname,lastname,email,password} = req.body
-	if(!firstname || !lastname || !email || !password) {
+	const {firstname, lastname, email, password} = req.body
+
+	if (!firstname || !lastname || !email || !password) {
 		req.flash('registerError', 'All fields is required')
 		res.redirect('/register')
 		return
 	}
-	const candidate =await User.findOne({email})
-if(candidate) {
-	req.flash('registerError', 'User already exist')
-	res.redirect('/register')
-	return
-}
-	const hashedPassword = await bcrytp.hash(password,10)
+
+	const candidate = await User.findOne({email})
+
+	if (candidate) {
+		req.flash('registerError', 'User already exist')
+		res.redirect('/register')
+		return
+	}
+
+	const hashedPassword = await bcrypt.hash(password, 10)
 	const userData = {
 		firstName: firstname,
 		lastName: lastname,
@@ -73,9 +87,8 @@ if(candidate) {
 		password: hashedPassword,
 	}
 	const user = await User.create(userData)
-	const token = generateJwtToken(user._id)
-	res.cookie("token",token, {httpOnly:true ,secure:true})
-	console.log(user);
+	const token = generateJWTToken(user._id)
+	res.cookie('token', token, {httpOnly: true, secure: true})
 	res.redirect('/')
 })
 

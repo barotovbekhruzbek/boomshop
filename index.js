5801625960ee4e32712d6cb490b4d8980f275dbe
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-02-17 14:41:14 +0500

    all file

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import mongoose from 'mongoose'
 import flash from 'connect-flash'
 import session from 'express-session'
 import * as dotenv from 'dotenv'
+import varMiddleware from './middleware/var.js'
+import cookieParser from 'cookie-parser'
+
 import AuthRoutes from './routes/auth.js'
 import ProductsRoutes from './routes/products.js'
 dotenv.config()
@@ -14,12 +17,15 @@ const hbs = create({defaultLayout: 'main', extname: 'hbs'})
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', './views')
+app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 app.use(express.json())
+app.use(cookieParser())
 app.use(session({secret: 'bexa',resave:false,saveUninitialized:false}))
 
 app.use(flash())
+app.use(varMiddleware)
 
 app.use(AuthRoutes)
 app.use(ProductsRoutes)
---
+++ middleware/var.js
@@ -0,0 +1,8 @@
+export default function(req,res,next) {
+
+const isAuth =req.cookies.token ? true : false
+
+res.locals.token = isAuth
+
+next()
+}

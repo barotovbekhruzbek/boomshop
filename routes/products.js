486ef843bc3d7121925862f9c7659fb03caf55b3
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-02-18 19:25:15 +0500

    add

--- routes/products.js
+++ routes/products.js
@@ -15,10 +15,13 @@ router.get('/',async (req, res) => {
 	})
 })
 
-router.get('/products', (req, res) => {
+router.get('/products', async (req, res) => {
+	const user = req.userId ? req.userId.toString() : null
+	const myProducts = await Product.find({user}).lean()
 	res.render('products', {
 		title: 'Products | bexa',
 		isProducts: true,
+		myProducts:myProducts
 	})
 })
 

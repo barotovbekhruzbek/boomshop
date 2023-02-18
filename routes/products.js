647c7c580597f4e47b9ffbca2bbe7ef20b8e36b1
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-02-18 12:15:30 +0500

    add

--- routes/products.js
+++ routes/products.js
@@ -19,10 +19,18 @@ router.get('/add', (req, res) => {
 	res.render('add', {
 		title: 'Add products',
 		isAdd: true,
+		errorAddProducts:req.flash('errorAddProducts')
 	})
 })
 router.post('/add-products', async (req,res) => {
 const {title,description,image,price} = req.body
+
+if(!title || !description || !image || !price) {
+	req.flash('errorAddProducts', 'All fields is required')
+	res.redirect('/add')
+	return
+}
+
 const products = await Product.create(req.body)
 console.log(products);
 	res.redirect('/')

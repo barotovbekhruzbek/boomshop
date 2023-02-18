f21a3378e5be7b98542eb7efb79416dfb49b2994
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-02-18 18:49:03 +0500

    helpers

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ import mongoose from 'mongoose'
 import flash from 'connect-flash'
 import session from 'express-session'
 import varMiddleware from './middleware/var.js'
+import userMiddleware from './middleware/user.js'
 import cookieParser from 'cookie-parser'
 import * as dotenv from 'dotenv'
 
 // ROUTES
 import AuthRoutes from './routes/auth.js'
 import ProductsRoutes from './routes/products.js'
+import helperUtilit from './utils/index.js'
 
 dotenv.config()
 
 const app = express()
 
-const hbs = create({defaultLayout: 'main', extname: 'hbs'})
+const hbs = create({defaultLayout: 'main', extname: 'hbs', helpers :helperUtilit})
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
@@ -28,6 +30,7 @@ app.use(cookieParser())
 app.use(session({secret: 'Sammi', resave: false, saveUninitialized: false}))
 app.use(flash())
 app.use(varMiddleware)
+app.use(userMiddleware)
 
 app.use(AuthRoutes)
 app.use(ProductsRoutes)

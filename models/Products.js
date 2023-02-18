77a9593bd2657799c0381d747ecb832efb18aece
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-02-18 11:13:03 +0500

    mongo db products create

--- models/Products.js
+++ models/Products.js
@@ -4,7 +4,8 @@ const ProductSchema = new Schema({
  title: {type:String,required:true},
  description: {type:String,required:true},
  image: {type:String,required:true},
- price: {type:Number,required:true}
+ price: {type:Number,required:true},
+ user: {type: Schema.Types.ObjectId, ref: 'User'}
 
 }, {
  timestamps:true
